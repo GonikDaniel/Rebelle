b0c863c7aecb45d31576fa05f859a848e8ca9deb
Gonik Daniel <gonik.daniel88@gmail.com>
2017-07-05 01:27:28 +0300

    Material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { WikiModule } from './wiki/wiki.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdCoreModule,
+  MdInputModule,
+  MdCardModule,
+  MdProgressSpinnerModule,
+  MdIconModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdCoreModule,
+    MdInputModule,
+    MdCardModule,
+    MdProgressSpinnerModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
