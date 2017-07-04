0ede22efef3377a91147bb1c10bdbefb9b9b6110
Gonik Daniel <gonik.daniel88@gmail.com>
2017-07-05 02:00:07 +0300

    Callback from search - right data flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-import { WikiModule } from './wiki/wiki.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +12,7 @@ import {
   MdProgressSpinnerModule,
   MdIconModule
 } from '@angular/material';
+
 import { SearchComponent } from './search/search.component';
 import { ListComponent } from './list/list.component';
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'rebelle-search',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  @Output() onSearch = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search(searchString) {
+    console.log(searchString);
+    this.onSearch.emit(searchString);
   }
 
 }
